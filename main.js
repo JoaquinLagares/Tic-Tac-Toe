b1928fb1c8f42e10c2998f18fe26b1352a613a95
JoaquinLagares <joaquinlagaresgarcia@gmail.com>
2026-07-21 20:47:05 -0300

    fix empty player name when pushing spacebar on players name

--- main.js
+++ main.js
@@ -88,6 +88,7 @@ const DisplayController = (() => {
     });
   });
 
+
   restartBtn.addEventListener("click", () => {
     Game.start("Player 1", "Player 2");
     render();
@@ -103,8 +104,8 @@ const message = document.querySelector("#message");
 const cells = document.querySelectorAll(".cell");
 
 startBtn.addEventListener("click", () => {
-  const p1 = document.querySelector("#player1")?.value || "Player 1";
-  const p2 = document.querySelector("#player2")?.value || "Player 2";
+  const p1 = document.querySelector("#player1")?.value.trim() || "Player 1";
+  const p2 = document.querySelector("#player2")?.value.trim()|| "Player 2";
 
   Game.start(p1, p2);   
   message.textContent = `${p1} vs ${p2} — Game started!`;
@@ -112,8 +113,8 @@ startBtn.addEventListener("click", () => {
 });
 
 restartBtn.addEventListener("click", () => {
-  const p1 = document.querySelector("#player1").value || "Player 1";
-  const p2 = document.querySelector("#player2").value || "Player 2";
+  const p1 = document.querySelector("#player1").value.trim() || "Player 1";
+  const p2 = document.querySelector("#player2").value.trim() || "Player 2";
 
   Game.start(p1, p2);
   message.textContent = "Game restarted!";
